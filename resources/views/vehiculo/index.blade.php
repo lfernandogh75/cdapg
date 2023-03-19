825be1f5f242316b9d41123ba7535a6e05bc6405
Luis Gallego <fernandogh75@misena.edu.co>
2023-03-19 12:27:42 -0500

    css y js directos de la pagina

--- resources/views/vehiculo/index.blade.php
+++ resources/views/vehiculo/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.plantillaperitaje')
 @section('css')
-<link href="/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 @endsection
 
 @section('contenido')
@@ -399,9 +400,9 @@
 
 @section('js')
  
-<script src="/dist/css/bootstrap.js">
 
-</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 
 @endsection
 @endsection
