fe63f51b6529477f3e238800850fd4d864a215e1
Luis Gallego <fernandogh75@misena.edu.co>
2023-03-25 13:30:36 -0500

    comoelprincipio

--- resources/views/vehiculo/myPDF.blade.php
+++ resources/views/vehiculo/myPDF.blade.php
@@ -81,7 +81,7 @@
   font-size:10px;
   margin-top:-19px;
    margin-left:20px;
- ">   <img src="iconos/directions_car.png"> N° INSPECCION {{$peritaje->id}}</p>
+ ">    src="{{ url('./iconos/directions_car.png')}}"> N° INSPECCION {{$peritaje->id}}</p>
       
        
         
